7788a38244302bb92b45feabb964283736fce783
MdFazleRasool <fazleamity001@gmail.com>
2025-04-03 15:41:05 +0530

    Introducing Props

---
+++ 001React/02Hooks_Props/02TailwindProps/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Card from './Card'
+
+function App() {
+
+  return (
+    < >
+      <h1   className='bg-green-400  p-4 rounded-xl text-black  mb-4'> Tailwind  test
+      </h1>
+
+      <Card />
+    </>
+  )
+}
+
+export default App
