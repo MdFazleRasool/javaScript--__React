1c964ac4feddf6fe9b4475b0f664bafc034674a6
MdFazleRasool <fazleamity001@gmail.com>
2025-04-04 09:48:26 +0530

    Custom hook

---
+++ 001React/02Hooks_Props/05currencyConverter/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <h1 className="text-3xl font-bold bg-orange-500 text-white">
+      Currency Converter
+    </h1>
+  )
+}
+
+export default App
