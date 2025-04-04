47a5a0f4cf71d14cbab2c9ad53ef876fe7f870b1
MdFazleRasool <fazleamity001@gmail.com>
2025-04-05 00:34:59 +0530

    Now I will Add User

--- 001React/03Router_ContextApi_Reduc/07reactRouter/src/App.jsx
+++ 001React/03Router_ContextApi_Reduc/07reactRouter/src/App.jsx
@@ -1,12 +1,21 @@
 
 import './App.css'
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
 
   return(
-    <h1 className='bg-gray-600 p-4'>
-      React Router
-    </h1>
+    <>
+      <h1 className='bg-gray-600 p-4'>
+        React Router
+      </h1>
+      <Header />
+      <Footer />
+      <Home />
+      
+    </>
   );
 }
 
