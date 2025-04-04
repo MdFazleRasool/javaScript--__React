54125da4737a123910bd6d5792d8c1ef17ee4480
MdFazleRasool <fazleamity001@gmail.com>
2025-04-04 10:46:02 +0530

    useId , (hooks) and componennt created

--- 001React/02Hooks_Props/05currencyConverter/src/App.jsx
+++ 001React/02Hooks_Props/05currencyConverter/src/App.jsx
@@ -1,13 +1,17 @@
 
 import './App.css'
+import InputBox from './components/InputBox'
 
 function App() {
 
 
   return (
-    <h1 className="text-3xl font-bold bg-orange-500 text-white">
-      Currency Converter
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold bg-orange-500 text-white">
+        Currency Converter
+      </h1>
+      <InputBox />
+    </>
   )
 }
 
