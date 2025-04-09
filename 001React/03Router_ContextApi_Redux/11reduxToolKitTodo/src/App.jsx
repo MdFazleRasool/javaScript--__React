7526666b4b4aebed2f5495716b67cf60344e04cd
MdFazleRasool <fazleamity001@gmail.com>
2025-04-09 09:29:03 +0530

    redux toolkit completed

--- 001React/03Router_ContextApi_Redux/11reduxToolKitTodo/src/App.jsx
+++ 001React/03Router_ContextApi_Redux/11reduxToolKitTodo/src/App.jsx
@@ -1,9 +1,13 @@
+import AddTodo from "./components/AddTodo"
+import Todos from "./components/Todos"
 
 function App() {
 
   return (
     <>
-      <h1>hello</h1>
+      <h1 className="bg-gray-700">Learning Redux Toolkit</h1>
+      <AddTodo />
+      <Todos />
     </>
   )
 }
