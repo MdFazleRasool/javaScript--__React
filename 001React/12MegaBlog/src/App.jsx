b8716f697e0615ab9bafed587f88e5c47df036f1
MdFazleRasool <fazleamity001@gmail.com>
2025-07-19 13:44:02 +0530

    Mega Project

--- 001React/12MegaBlog/src/App.jsx
+++ 001React/12MegaBlog/src/App.jsx
@@ -1,10 +1,39 @@
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux";
+import {login,logout} from './store/authSlice'
+import {Header , Footer } from './components'
+import authService from './appwrite/auth'
 
 function App() {
-  return (
-    <>
-      <h1>A Blog App With  Appwrite</h1>
-    </>
-  )
+  
+  const [loading , setLoading] = useState(true);
+  const dispatch  = useDispatch ();
+
+  useEffect (()=>{
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData) 
+        dispatch(login({userData}))
+      else 
+      dispatch(logout())
+    })
+    .finally(() => setLoading(false))
+  },[])
+ 
+
+
+
+  return !loading ? (
+    <div className="min-h-sc flex flex-wrap content-between bg-gray-400" >
+      <div  className="w-full block">
+        <Header />
+        <main>
+          Todo : {/* {Outlet} */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
